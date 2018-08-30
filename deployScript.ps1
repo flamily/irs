3a12aec559642b16486d5b52e0cc243ba71db8eb
David Niwczyk <david.niwczyk@rockend.com.au>
2018-08-30 12:10:26 +1000

    Added deploy script and ARM template

---
+++ deployScript.ps1
@@ -0,0 +1,5 @@
+﻿$resourceGroupName = "travisTest"
+$templatePath = "C:\Users\david\Documents\ictd_test\templates\appService.Json"
+
+New-AzureRmResourceGroup -Name $resourceGroupName -Location "Australia East" -Verbose -Force
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile $templatePath 
