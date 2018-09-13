aa22712239c36441140e8fff591b27aa6c7c7c84
arosspope <andrew.pope456@gmail.com>
2018-09-13 18:40:22 +1000

    wip on staff table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,18 @@
 -- Schema Enums
 CREATE TYPE event_e AS ENUM ('ready', 'seated', 'paid', 'maintaining');
+CREATE TYPE permission_e AS ENUM ('robot', 'wait_staff', 'management');
+CREATE TYPE shape_e AS ENUM ('rectangle', 'ellipse');
+
+-- Schema Tables
+CREATE TABLE staff (
+	staff_id		serial			PRIMARY KEY,
+	username		text 				NOT NULL UNIQUE,
+	first_name 	text 				NOT NULL,
+	last_name 	text 				NOT NULL,
+	password		varchar()		NOT NULL,
+	start_dt		timestamp		NOT NULL,
+	permission	permission_e NOT NULL
+); --WIP
 
 create table sentiments (
 	    id SERIAL primary key,
