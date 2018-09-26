d9780d6cd7f7f83def73a1ffb4ade2f2705ec6e2
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-09-27 01:04:03 +1000

    add flake8

--- test/conftest.py
+++ test/conftest.py
@@ -2,4 +2,4 @@
 Include test fixtures here for auto-discovery
 """
 # pylint: disable=unused-import
-from irs.test.fixtures.db_connection import db_connection
+from irs.test.fixtures.db_connection import db_connection  # noqa: F401
--- test/fixtures/db_connection.py
+++ test/fixtures/db_connection.py
@@ -12,12 +12,15 @@ import psycopg2
 # after tests:
 # nuke that database
 
-def connection_string(): #pragma: no cover
+
+def connection_string():  # pragma: no cover
     if os.environ.get("TRAVIS", False):
-        return "user='postgres' host='localhost'" #pragma: no cover
+        return "user='postgres' host='localhost'"  # pragma: no cover
     return "user='postgres' host='localhost' password='mysecretpassword'"
+
 
 @pytest.fixture()
 def db_connection():
-    conn = psycopg2.connect(connection_string()) # this might blow up, but that is ok in tests.
+    # this might blow up, but that is ok in tests.
+    conn = psycopg2.connect(connection_string())
     yield conn
