6a3700aae01e00e7358ed7e49a19f73b33e11bd9
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-10-04 10:29:34 +1000

    Add Database Tests (#13)
    * bing
    
    * pip is being a bitch
    
    * python++
    
    * how do dictionaries even work?
    
    * test for the boys
    
    * grumpy coverage
    
    * more spice
    
    * another try?
    
    * better?
    
    * bingo
    
    * are you entertained?
    
    * more readme
    
    * remove flaskiness
    
    * add flake8
    
    * wild ideas
    
    * wew
    
    * starting to look like business
    
    * travis relax
    
    * haha, you can't merge this with a todo in it
    
    * look at me now mum
    
    * "Your code has been rated at 10.00/10"
    
    * Addressing the review comments

---
+++ test/fixtures/db_connection.py
@@ -0,0 +1,78 @@
+'''
+Purpose Create a database for running unit tests
+Author Robin Wohlers-Reichel
+Date 2018-10-04
+'''
+
+import os
+import uuid
+
+import pytest
+import psycopg2
+from psycopg2 import pool
+
+
+# Start of test:
+# connect to box
+# create new database with guid name
+# apply schema
+# add test values
+
+# after tests:
+# nuke that database
+
+
+def connection_string():  # pragma: no cover
+    if os.environ.get("TRAVIS", False):
+        return "user='postgres' host='localhost'"  # pragma: no cover
+    return "user='postgres' host='localhost'"
+
+
+@pytest.fixture(scope="session")
+def database():
+    identifier = uuid.uuid4().hex
+    db_name = "irs_{}".format(identifier)
+    schema_name = "irs_schema_{}".format(identifier)
+    full_connection = "{} dbname={}".format(connection_string(), db_name)
+
+    # create the database
+    conn = psycopg2.connect(connection_string())
+    conn.autocommit = True
+    with conn.cursor() as cur:
+        cur.execute('CREATE DATABASE {};'.format(db_name))
+    conn.close()
+
+    # apply the schema
+    conn = psycopg2.connect(full_connection)
+    conn.autocommit = True
+    with conn.cursor() as cur:
+        cur.execute('CREATE SCHEMA {};'.format(schema_name))
+        with open("db/schema.sql", 'r') as schema:
+            cur.execute(schema.read())
+    conn.close()
+
+    # create a pool 🏊‍♂️
+    # to run parallel tests:
+    # - increase max number of connections
+    # - change to ThreadedConnectionPool
+    db_pool = pool.SimpleConnectionPool(1, 1, full_connection)
+    yield db_pool
+    db_pool.closeall()
+
+    # cleanup
+    # have to make a new connection to the 'postgres' (default) database
+    #  as we are not allowed to drop the current database (irs_####).
+    conn = psycopg2.connect(connection_string())
+    conn.autocommit = True
+    with conn.cursor() as cur:
+        cur.execute('DROP DATABASE {};'.format(db_name))
+    conn.close()
+
+
+@pytest.fixture()
+def db_connection(database):
+    # pylint:disable=redefined-outer-name
+    conn = database.getconn()
+    yield conn
+    conn.rollback()
+    database.putconn(conn)
--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
-def inc(x):
-    return x + 1
+def inc(start):
+    return start + 1
 
 
 def test_answer():
     assert inc(3) == 4
+
+
+def test_ass():
+    assert inc(4) == 5
