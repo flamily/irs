ccb57a8056045f56f8ad95ba1702149169157f4e
arosspope <andrew.pope456@gmail.com>
2018-10-05 14:01:54 +1000

    fixing flake8 complaints

--- test/conftest.py
+++ test/conftest.py
@@ -4,4 +4,4 @@ Include test fixtures here for auto-discovery
 # pylint: disable=unused-import
 from irs.test.fixtures.db_connection import db_connection  # noqa: F401
 from irs.test.fixtures.db_connection import database  # noqa: F401
-from irs.test.fixtures.db_connection import database_snapshot # noqa: F401
+from irs.test.fixtures.db_connection import database_snapshot  # noqa: F401
