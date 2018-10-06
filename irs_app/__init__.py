be0ecd95cfca51b72aa61dc9fdaee552fdbc8baa
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-10-06 16:42:56 +1000

    poke the bear

--- irs_app/__init__.py
+++ irs_app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import redirect, url_for
+from flask import redirect, url_for, send_from_directory
 
 import irs.irs_app.db
 app = Flask(__name__)
 
 db.register(app)
 
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('templates/static/js', path)
+
+@app.route('/vendor/<path:path>')
+def send_vendor(path):
+    return send_from_directory('templates/static/vendor', path)
+
 @app.route('/')
-@app.route('/<name>')
 def hello_world(name=None):
     return render_template('index.html', name=name)
 
--- irs_app/db.py
+++ irs_app/db.py
@@ -4,21 +4,16 @@ from flask.cli import with_appcontext
 from psycopg2 import pool
 from irs import config
 
+__pool = pool.ThreadedConnectionPool(1, 5, config.connection_string())
+
 
 def register(app):
-    app.teardown_request(__teardown_db_conn)
-    app.teardown_appcontext(__teardown_db_pool)
-
-
-def get_db_pool():
-    if 'db_pool' not in g:
-        g.db = pool.SimpleConnectionPool(1, 5, config.connection_string())
-    return g.db
+    app.teardown_appcontext(__teardown_db_conn)
 
 
 def get_db_conn():
     if 'db_conn' not in g:
-        g.db_conn = get_db_pool().getconn()
+        g.db_conn = __pool.getconn()
     return g.db_conn
 
 
@@ -27,13 +22,7 @@ def __teardown_db_conn(exception=None):
     db_conn = g.pop('db_conn', None)
     if db_conn is not None:
         if exception is not None:
+            print('exception, rolling back transaction')
             db_conn.rollback()
-        get_db_pool().putconn(db_conn)
-
-
-# called when the app is shutting down
-def __teardown_db_pool():
-    print("close all the db connections")
-    db = g.pop('db', None)
-    if db is not None:
-        db.closeall()
+        db_conn.commit()
+        __pool.putconn(db_conn)
