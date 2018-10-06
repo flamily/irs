924e4d110c1c5b4f7f285f7d24400a34dc63a1aa
chrisstime <christine.vinaviles@gmail.com>
2018-10-06 15:55:37 +1000

    add more page routes

--- routes.py
+++ routes.py
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/users')
+def users():
+    return render_template('tables.html')
+
+
+@app.route('/reports')
+def reports():
+    return render_template('charts.html')
+
+
+@app.route('/error-404')
+def error():
+    return render_template('404.html')
