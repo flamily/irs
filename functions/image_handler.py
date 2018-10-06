51f35c05a7a721708c1f5228c5222f12c741f6c6
David Niwczyk <david.niwczyk@rockend.com.au>
2018-10-06 21:05:38 +1000

    bit of cleanup and added cognitive_face library

--- functions/image_handler.py
+++ functions/image_handler.py
@@ -7,15 +7,12 @@ import cognitive_face as CF
 print('Loading function')
 s3 = boto3.client('s3')
 
-def lambda_handler(event, context):
+def emotion_recognition(event, context):
     
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
-        bucket_location = s3.get_bucket_location(Bucket=bucket)
-        
+
         url = s3.generate_presigned_url(
             ClientMethod='get_object',
             ExpiresIn='10',
@@ -30,7 +27,7 @@ def lambda_handler(event, context):
         face = CF.face.detect(url)
         print(face)
 
-        return response['ContentType']
+        return face
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
