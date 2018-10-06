7eb4ac42ed60452a2cda897073b90ec437c8ded5
chrisstime <christine.vinaviles@gmail.com>
2018-10-06 17:10:07 +1000

    pylint is complaining

--- routes.py
+++ routes.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
 
-@app.route('/')
+@APP.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@APP.route('/login')
 def login():
     return render_template('login.html')
 
 
-@app.route('/users')
+@APP.route('/users')
 def users():
     return render_template('tables.html')
 
 
-@app.route('/reports')
+@APP.route('/reports')
 def reports():
     return render_template('charts.html')
 
 
-@app.route('/error-404')
+@APP.route('/error-404')
 def error():
     return render_template('404.html')
