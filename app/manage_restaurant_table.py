f90e212e93db891698f58489ab6cbead2c771632
arosspope <andrew.pope456@gmail.com>
2018-10-06 13:16:49 +1000

    specifying enums

--- app/manage_restaurant_table.py
+++ app/manage_restaurant_table.py
@@ -21,6 +21,20 @@ class Shape(Enum):
         return self.value
 
 
+class Event(Enum):
+    """An enum that specifies the events that occur at a restaurant table."""
+
+    ready = 'ready'
+    seated = 'seated'
+    attending = 'attending'
+    paid = 'paid'
+    maintaining = 'maintaining'
+
+    def __str__(self):
+        """Return string version of enum."""
+        return self.value
+
+
 class State(Enum):
     """An enum that specifies the various states of a restaurant table."""
 
@@ -32,14 +46,28 @@ class State(Enum):
         """Return string version of enum."""
         return self.value
 
+    @staticmethod
+    def resolve_state(recent_event):
+        """Resolve a state based on the most recent event."""
+        if recent_event is Event.ready:
+            return State.available
+        elif recent_event is (Event.seated or Event.attending):
+            return State.occupied
+        elif recent_event is (Event.maintaining or Event.paid):
+            return State.unavailable
+        else:
+            raise Exception('Unknown event')
 
 
 class RestaurantTable():
     """Object version of restaurant table db record."""
 
-    def __init__(self, shape, coordinate, state, capacity):
+    def __init__(self, id, shape, coordinate, width, height, state, capacity):
         """Create a restaurant table."""
-        self.shape = Shape(shape)
+        self.id = id
+        self.shape = Shape(str(shape))  # You can pass the enum, or a string!
         self.coordinate = None  # Coordinate(0, 0)
-        self.state = None
-        self.capacity = None
+        self.state = State(str(state))
+        self.capacity = capacity
+        self.width = width
+        self.height = height
