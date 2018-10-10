8d35bdf328720b4c791eb02f42ef4aa25e09b748
David Niwczyk <david.niwczyk@rockend.com.au>
2018-10-11 10:28:07 +1100

    wew

--- app/emotion_recognition.py
+++ app/emotion_recognition.py
@@ -2,35 +2,39 @@ import cognitive_face as CF
 import locale
 import os
 
+
 class SatisfactionScore:
-	def __init__(self):
-		self._base_url = os.environ['EMOTION_API_BASE_URL']
-		KEY = os.environ['EMOTION_API_KEY']
-		self._cognitive_face = CF
-		self._cognitive_face.Key.set(KEY)
-		self._cognitive_face.BaseUrl.set(self._base_url)
+    def __init__(self):
+        self._base_url = os.environ['EMOTION_API_BASE_URL']
+        KEY = os.environ['EMOTION_API_KEY']
+        self._cognitive_face = CF
+        self._cognitive_face.Key.set(KEY)
+        self._cognitive_face.BaseUrl.set(self._base_url)
 
-	def detect_from_url(self, url):
-		return self.detect(url)
+    def detect_from_url(self, url):
+        return self.detect(url)
 
-	def detect_from_local_file(self, filepath):
-		#with open(filepath, "rb") as image:
-	#		image_as_bytes = image.read()
-		locale.getdefaultlocale()
+    def detect_from_local_file(self, filepath):
+        # with open(filepath, "rb") as image:
+        #        image_as_bytes = image.read()
+        locale.getdefaultlocale()
 
-		f = open(filepath, "rb")
-		image_as_bytes = f.read()
-		f.close()
+        f = open(filepath, "rb")
+        image_as_bytes = f.read()
+        f.close()
 
-		return self.detect(image_as_bytes)
+        return self.detect(image_as_bytes)
 
-	def detect(self, image, face_id=True, landmarks=False, attributes='emotion,glasses'):
-		return self._cognitive_face.face.detect(image, face_id, landmarks, attributes)
+    def detect(self, image, face_id=True,
+               landmarks=False, attributes='emotion,glasses'):
+        return self._cognitive_face.face.detect(
+            image, face_id, landmarks, attributes
+            )
 
 
 if __name__ == "__main__":
-	css = SatisfactionScore()
+    css = SatisfactionScore()
 
-	#print(css.detect_from_url('https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'))
+    # print(css.detect_from_url('https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'))
 
-	print(css.detect_from_local_file("face.jpg"))
+    print(css.detect_from_local_file("face.jpg"))
--- app/satisfaction_lambda.py
+++ app/satisfaction_lambda.py
@@ -1,16 +1,17 @@
-import json
 import urllib.parse
 import boto3
-import os
 from emotion_recognition import SatisfactionScore
 
 print('Loading function')
 s3 = boto3.client('s3')
 
+
 def customer_satisfaction(event, context):
-    
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    key = urllib.parse.unquote_plus(
+        event['Records'][0]['s3']['object']['key'],
+        encoding='utf-8'
+    )
     try:
 
         url = s3.generate_presigned_url(
@@ -28,5 +29,7 @@ def customer_satisfaction(event, context):
         return css
     except Exception as e:
         print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        print('Error getting object {} from bucket {}.\
+         Make sure they exist and your bucket is in the \
+         same region as this function.'.format(key, bucket))
         raise e
