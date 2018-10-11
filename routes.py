552cb6d56bf30c000cd16e6bd67543e646b5d026
chrisstime <christine.vinaviles@gmail.com>
2018-10-11 11:32:27 +1100

    standardise html headers and breadcrumbs

--- routes.py
+++ routes.py
@@ -6,28 +6,48 @@ APP = Flask(__name__)
 @APP.route('/')
 def index():
     page_title = 'IRS - Dashboard'
-    return render_template('index.html', **locals())
+    breadcrumb_title = ''
+    return render_template(
+        'index.html',
+        page_title=page_title,
+        breadcrumb_title=breadcrumb_title
+    )
 
 
 @APP.route('/login')
 def login():
     page_title = 'Intelligent Restaurant System - Login'
-    return render_template('login.html')
+    return render_template('login.html', page_title=page_title)
 
 
 @APP.route('/users')
 def users():
     page_title = 'IRS - Users'
-    return render_template('tables.html')
+    breadcrumb_title = 'Users'
+    return render_template(
+        'tables.html',
+        page_title=page_title,
+        breadcrumb_title=breadcrumb_title
+    )
 
 
 @APP.route('/reports')
 def reports():
     page_title = 'IRS - Reports'
-    return render_template('charts.html')
+    breadcrumb_title = 'Reports'
+    return render_template(
+        'charts.html',
+        page_title=page_title,
+        breadcrumb_title=breadcrumb_title
+    )
 
 
 @APP.route('/error-404')
 def error():
-    page_title = 'SB Admin - 404 Error'
-    return render_template('404.html')
+    page_title = 'IRS - 404 Error'
+    breadcrumb_title = 'Error'
+    return render_template(
+        '404.html',
+        page_title=page_title,
+        breadcrumb_title=breadcrumb_title
+    )
