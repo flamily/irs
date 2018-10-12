c91b32d53f8cba22befb66e76afa794245268218
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-10-12 13:47:56 +1100

    still messing up merges

--- web/__init__.py
+++ web/__init__.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask
 
 from irs.web.db import register as register_db
-from irs.web.friend import friend_blueprint
 from irs.web.login import login_blueprint
 from irs.web.index import index_blueprint
 
@@ -11,6 +10,5 @@ app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
 register_db(app)
-app.register_blueprint(friend_blueprint, url_prefix="/friend")
 app.register_blueprint(login_blueprint, url_prefix="/login")
 app.register_blueprint(index_blueprint)
