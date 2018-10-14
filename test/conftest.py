26a6a86ba55eb5647ded5c243c2ae6ae09c4ca10
chrisstime <christine.vinaviles@gmail.com>
2018-10-14 11:54:29 +1100

    revert conftest.py changes and add pylint disable because I'm desperate

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,9 @@
 """
 Include test fixtures here for auto-discovery
 """
+# pylint: disable=unused-import
+from irs.test.fixtures.db_connection import db_connection  # noqa: F401
+from irs.test.fixtures.db_connection import database  # noqa: F401
+from irs.test.fixtures.db_connection import database_snapshot  # noqa: F401
+from irs.test.fixtures.flask_app import app  # noqa: F401
+from irs.test.fixtures.flask_app import client  # noqa: F401
