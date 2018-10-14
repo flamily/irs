e2bdb90b97f3bceb1cf2d5bc2b331b86b8b2e3ad
chrisstime <christine.vinaviles@gmail.com>
2018-10-14 11:44:39 +1100

    experiments

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,3 @@
 """
 Include test fixtures here for auto-discovery
 """
-from irs.test.fixtures.db_connection import db_connection
-from irs.test.fixtures.db_connection import database
-from irs.test.fixtures.db_connection import database_snapshot
-from irs.test.fixtures.flask_app import app
-from irs.test.fixtures.flask_app import client
