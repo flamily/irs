b17399d3b96123ac6e40711686e2f62c995d7151
chrisstime <christine.vinaviles@gmail.com>
2018-10-14 11:36:42 +1100

    readd conftest imports

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,8 @@
 """
 Include test fixtures here for auto-discovery
 """
+from irs.test.fixtures.db_connection import db_connection
+from irs.test.fixtures.db_connection import database
+from irs.test.fixtures.db_connection import database_snapshot
+from irs.test.fixtures.flask_app import app
+from irs.test.fixtures.flask_app import client
