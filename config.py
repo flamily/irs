8f61dc3b4ec0293d466fba562a40a7f6ddcd8b7e
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-10-15 12:31:53 +1100

    ???????????????? (#61)

--- config.py
+++ config.py
@@ -3,5 +3,7 @@ import os
 
 def connection_string():  # pragma: no cover
     if os.environ.get("TRAVIS", False):
-        return "user='postgres' host='localhost'"  # pragma: no cover
+        return "user='postgres' host='localhost'"
+    if os.environ.get("DATABASE_URL", False):
+        return os.environ["DATABASE_URL"]
     return "user='postgres' host='localhost'"
