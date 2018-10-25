7d676a20b1a4dc4d49e8f18dbd98128e40411aab
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-10-25 11:02:46 +1100

    get access to the testing pool

--- test/fixtures/flask_app.py
+++ test/fixtures/flask_app.py
@@ -5,10 +5,13 @@ import web
 @pytest.fixture
 def app(database_snapshot):
     web.APP.config['TESTING_DB_POOL'] = database_snapshot
+    web.APP.testing_db_pool = database_snapshot
     return web.APP
 
 
 @pytest.fixture
 def client(app):
     # pylint:disable=redefined-outer-name
-    return app.test_client()
+    client = app.test_client()
+    client.testing_db_pool = app.testing_db_pool
+    return client
