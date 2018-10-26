8fb0d6dc5f4f719c9164f82f2e8ddc9770a50cb9
Christine Vinaviles <christine.vinaviles@gmail.com>
2018-10-27 00:19:09 +1100

    setup blueprint and initial html things

--- web/__init__.py
+++ web/__init__.py
@@ -4,9 +4,11 @@ from flask import Flask
 from web.db import register as register_db
 from web.login import LOGIN_BLUEPRINT
 from web.index import INDEX_BLUEPRINT
+from web.welcome import WELCOME_BLUEPRINT
 
 APP = Flask(__name__)
 APP.secret_key = os.urandom(16)
 register_db(APP)
 APP.register_blueprint(LOGIN_BLUEPRINT, url_prefix="/login")
 APP.register_blueprint(INDEX_BLUEPRINT)
+APP.register_blueprint(WELCOME_BLUEPRINT)
