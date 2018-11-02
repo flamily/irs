a80b78e004b82074efd9b06fc75b2709bd59199c
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-02 13:06:31 +1100

    bing bing

--- web/static/js/pa.js
+++ web/static/js/pa.js
@@ -1,32 +1,33 @@
-var irs = {}
-if (robot_available()) {
-    irs = {
-        identify: function() {return 1}
+var irs = function() {
+    function robot_available() {
+        //check if we are running on the robot
+        typeof irs_raw != "undefined"
     }
-} else if (chrome_available()) {
-    irs = {
-        photo: photo,
-        listen: listen,
-        say: say,
-        identify: function() {return 2}
+
+    function chrome_available() {
+        //check all the apis are available
+        return true
     }
-} else {
-    irs = {
-        identify: function() {return 0}
+
+    /* are we on the robot? */
+    if (robot_available()) {
+        /* setup the robot callback handlers */
+        return {
+            identify: function() {return 1}
+        }
+    } else if (chrome_available()) {
+        return {
+            photo: function() {},
+            listen: function() {},
+            say: function(phrase, callback) {
+                alert(phrase)
+                callback()
+            },
+            identify: function() {return 2}
+        }
+    } else {
+        return {
+            identify: function() {return 0}
+        }
     }
-}
-
-function robot_available() {
-    //check if we are running on the robot
-    return false
-}
-
-function chrome_available() {
-    //check all the apis are available
-    return true
-}
-
-function say(phrase, callback) {
-    alert(phrase)
-    callback()
-}
+}()
