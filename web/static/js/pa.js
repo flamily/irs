f3cff796a415ca26db9e039c3d588cd10e5a5425
gav0 <jgav95@gmail.com>
2018-11-02 14:51:46 +1100

    Update web/static/js/pa.js
    +wiretap
    +spycam
    -spaghet

--- web/static/js/pa.js
+++ web/static/js/pa.js
@@ -1,8 +1,58 @@
 var irs = (function() {
+	
+	//Voice Recognition
     function listen(words, timeout, callback) {
+		
+		var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+		var recognition = new SpeechRecognition();
+		
+		recognition.continuous = true;
+		
+		recognition.onresult = function(event) {
 
+			txt = event.results[event.resultIndex][0].transcript
+			console.log(txt);
+
+			recognised = txt.split(" ")
+
+			for (word in recognised){
+				x = words.indexOf(recognised[word]);
+				if (x!=-1){
+					console.log(words.indexOf(recognised[word]));
+				}			
+			
+			}
+			
+		}
+		
+		recognition.start();
+		setTimeout(function () {recognition.stop();console.log("lalalala I can't hear you")}, 10000);
 
     }
+	
+	//Image Capture Code
+	function gotMedia(mediaStream) {
+	mediaStreamTrack = mediaStream.getVideoTracks()[0];
+	imageCapture = new ImageCapture(mediaStreamTrack);
+	console.log(imageCapture);
+	
+	}
+
+	function takePhoto(callback){
+		
+		console.log("Say cheese")
+		imageCapture.grabFrame()
+	  .then(imageBitmap => {
+		callback(imageBitmap);
+	  })
+	  .catch(error => console.log(error));
+	}
+
+	navigator.mediaDevices.getUserMedia({video: true})
+	  .then(gotMedia)
+	  .catch(error => console.error('getUserMedia() error:', error));
+	  
+	  
 
     function robot_available() {
         //check if we are running on the robot
@@ -29,6 +79,7 @@ var irs = (function() {
         return {
             photo: function() {},
             listen: listen,
+			takePhoto: takePhoto,
             say: function(phrase) {
                 var msg = new SpeechSynthesisUtterance(phrase);
                 window.speechSynthesis.speak(msg);
