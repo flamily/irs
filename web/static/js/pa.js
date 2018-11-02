8424e6e1cd2d2191b649c56694db1865e70cb05f
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-02 14:52:26 +1100

    wow! spaghet

--- web/static/js/pa.js
+++ web/static/js/pa.js
@@ -1,31 +1,15 @@
 var irs = (function() {
-    function listen(words, timeout, callback) {
-
-
-    }
-
-    function robot_available() {
-        //check if we are running on the robot
-        typeof irs_raw != "undefined"
-    }
-
-    function chrome_available() {
-        //check all the apis are available
-        return true
-    }
-
-    /* are we on the robot? */
-    if (robot_available()) {
-        /* setup the robot callback handlers */
-        irs_raw.phraseSuccess = function(res) {
-            console.log(res);
+    function chrome() {
+        function chrome_available() {
+            //check all the apis are available
+            return true
         }
+        function listen(words, timeout, callback) {
 
-        /* make the irs thingo */
-        return {
-            identify: function() {return 1}
         }
-    } else if (chrome_available()) {
+        if (!chrome_available()) {
+            return null
+        }
         return {
             photo: function() {},
             listen: listen,
@@ -37,9 +21,22 @@ var irs = (function() {
                 return 2
             }
         }
-    } else {
+    }
+
+    function robot() {
+        //check if we are running on the robot
+        if(typeof irs_raw === "undefined") {
+            return null
+        }
+        /* setup the robot callback handlers */
+        irs_raw.phraseSuccess = function(res) {
+            console.log(res);
+        }
+
+        /* make the irs thingo */
         return {
-            identify: function() {return 0}
+            identify: function() {return 1}
         }
     }
+    return robot() || chrome() || {identify: function() {return 0}}
 })()
