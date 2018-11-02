f773b700031601dd0e3ea0aafaaba04f468b495f
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-02 15:07:39 +1100

    wew

--- web/static/js/pa.js
+++ web/static/js/pa.js
@@ -1,71 +1,80 @@
 var irs = (function() {
-	
-	//Voice Recognition
-    function listen(words, timeout, callback) {
-		
-		var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-		var recognition = new SpeechRecognition();
-		
-		recognition.continuous = true;
-		
-		recognition.onresult = function(event) {
+    function chrome() {
+        /* do some magical checking that chrome is available */
+        if (true===1) {
+            return null
+        }
 
-			txt = event.results[event.resultIndex][0].transcript
-			console.log(txt);
+        //Voice Recognition
+        function listen(words, timeout, callback) {
+            
+            var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            var recognition = new SpeechRecognition();
+            
+            recognition.continuous = true;
+            
+            recognition.onresult = function(event) {
 
-			recognised = txt.split(" ")
+                txt = event.results[event.resultIndex][0].transcript
+                console.log(txt);
 
-			for (word in recognised){
-				x = words.indexOf(recognised[word]);
-				if (x!=-1){
-					console.log(words.indexOf(recognised[word]));
-				}			
-			
-			}
-			
-		}
-		
-		recognition.start();
-		setTimeout(function () {recognition.stop();console.log("lalalala I can't hear you")}, 10000);
+                recognised = txt.split(" ")
 
+                for (word in recognised){
+                    x = words.indexOf(recognised[word]);
+                    if (x!=-1){
+                        console.log(words.indexOf(recognised[word]));
+                    }			
+                
+                }
+                
+            }
+            
+            recognition.start();
+            setTimeout(function () {recognition.stop();console.log("lalalala I can't hear you")}, 10000);
+
+        }
+        
+        //Image Capture Code
+        function gotMedia(mediaStream) {
+        mediaStreamTrack = mediaStream.getVideoTracks()[0];
+        imageCapture = new ImageCapture(mediaStreamTrack);
+        console.log(imageCapture);
+        
+        }
+
+        function takePhoto(callback){
+            
+            console.log("Say cheese")
+            imageCapture.grabFrame()
+        .then(imageBitmap => {
+            callback(imageBitmap);
+        })
+        .catch(error => console.log(error));
+        }
+
+        navigator.mediaDevices.getUserMedia({video: true})
+        .then(gotMedia)
+        .catch(error => console.error('getUserMedia() error:', error));
+
+        return {
+            photo: function() {},
+            listen: listen,
+            say: function(phrase) {
+                var msg = new SpeechSynthesisUtterance(phrase);
+                window.speechSynthesis.speak(msg);
+            },
+            identify: function() {
+                return 2
+            }
+        }
     }
-	
-	//Image Capture Code
-	function gotMedia(mediaStream) {
-	mediaStreamTrack = mediaStream.getVideoTracks()[0];
-	imageCapture = new ImageCapture(mediaStreamTrack);
-	console.log(imageCapture);
-	
-	}
 
-	function takePhoto(callback){
-		
-		console.log("Say cheese")
-		imageCapture.grabFrame()
-	  .then(imageBitmap => {
-		callback(imageBitmap);
-	  })
-	  .catch(error => console.log(error));
-	}
-
-	navigator.mediaDevices.getUserMedia({video: true})
-	  .then(gotMedia)
-	  .catch(error => console.error('getUserMedia() error:', error));
-	  
-	  
-
-    function robot_available() {
+    function robot() {
         //check if we are running on the robot
-        typeof irs_raw != "undefined"
-    }
-
-    function chrome_available() {
-        //check all the apis are available
-        return true
-    }
-
-    /* are we on the robot? */
-    if (robot_available()) {
+        if (typeof irs_raw == "undefined") {
+            return null
+        }
         /* setup the robot callback handlers */
         irs_raw.phraseSuccess = function(res) {
             console.log(res);
@@ -75,22 +84,6 @@ var irs = (function() {
         return {
             identify: function() {return 1}
         }
-    } else if (chrome_available()) {
-        return {
-            photo: function() {},
-            listen: listen,
-			takePhoto: takePhoto,
-            say: function(phrase) {
-                var msg = new SpeechSynthesisUtterance(phrase);
-                window.speechSynthesis.speak(msg);
-            },
-            identify: function() {
-                return 2
-            }
-        }
-    } else {
-        return {
-            identify: function() {return 0}
-        }
     }
+    return robot() || chrome() || {identify: function() {return 0}}
 })()
