00390d68a102494607fcd4489480dcff1af4ea1c
gav0 <jgav95@gmail.com>
2018-11-02 16:39:46 +1100

    Update web/static/js/pa.js
    hidden easter egg dataURL

--- web/static/js/pa.js
+++ web/static/js/pa.js
@@ -60,14 +60,34 @@ var irs = (function() {
         console.log(imageCapture);
         
         }
-
+		
+		function drawCanvas(canvas, img) {
+		  canvas.width = getComputedStyle(canvas).width.split('px')[0];
+		  canvas.height = getComputedStyle(canvas).height.split('px')[0];
+		  let ratio  = Math.min(canvas.width / img.width, canvas.height / img.height);
+		  let x = (canvas.width - img.width * ratio) / 2;
+		  let y = (canvas.height - img.height * ratio) / 2;
+		  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+		  canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
+			  x, y, img.width * ratio, img.height * ratio);
+		}
+		
+		
+		
         function takePhoto(callback){
             
             console.log("Say cheese")
-            imageCapture.grabFrame()
-        .then(imageBitmap => {
-            callback(imageBitmap);
-        })
+			
+			var htmlCanvas = document.createElement("canvas");
+			htmlCanvas.width = 1280;
+			htmlCanvas.height = 720;
+			htmlCanvas.style.visibility = "hidden";
+			document.body.appendChild(htmlCanvas);
+			imageCapture.grabFrame()
+			  .then(imageBitmap => {
+				drawCanvas(htmlCanvas, imageBitmap);
+				console.log(htmlCanvas.toDataURL())
+			  })
         .catch(error => console.log(error));
         }
 
