b453baa6c30c59ed3c45d3e3125e8e3ee328b951
NDGogo <andy.go@student.uts.edu.au>
2018-11-02 17:20:26 +1100

    flask routing from modal to tables

--- web/select_party_size.py
+++ web/select_party_size.py
@@ -1,18 +1,25 @@
-from flask import Blueprint
-
-from web.decorators import (
-    login_required, templated
-)
-
-
-# Reference for blueprints here:
-# http://flask.pocoo.org/docs/1.0/blueprints/
-SELECT_PARTY_SIZE_BLUEPRINT = Blueprint(
-    'select_party_size', __name__, template_folder='templates')
+# from flask import (
+#     redirect,
+#     url_for, Blueprint
+# )
+# from web.decorators import (
+#     login_required, templated
+# )
 
 
-@SELECT_PARTY_SIZE_BLUEPRINT.route('/select-party-size')
-@templated(template='select-party-size.html')
-@login_required()
-def index():
-    return dict(page_title='Robot - Select Party Size')
+# # Reference for blueprints here:
+# # http://flask.pocoo.org/docs/1.0/blueprints/
+# SELECT_PARTY_SIZE_BLUEPRINT = Blueprint(
+#     'select_party_size',__name__, template_folder='templates')
+
+
+# @SELECT_PARTY_SIZE_BLUEPRINT.route('/select-party-size')
+# @templated(template='select-party-size.html')
+# @login_required()
+# def index():
+#     return dict(page_title='Robot - Select Party Size')
+
+
+# @SELECT_PARTY_SIZE_BLUEPRINT.route('/tables', methods=['POST'])
+# def tables():
+#     return redirect(url_for('tables'))
