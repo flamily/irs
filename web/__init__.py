2c1ea5dd5e7f2419782a9db7b37c98da9170c104
John Pham <john@s1t2.com.au>
2018-11-03 19:02:43 +1100

    Fixed merge issues

--- web/__init__.py
+++ web/__init__.py
@@ -4,11 +4,11 @@ from flask import Flask
 from web.db import register as register_db
 from web.login import LOGIN_BLUEPRINT
 from web.index import INDEX_BLUEPRINT
-from web.table_status import TABLE_STATUS_BLUEPRINT
+from web.tables import TABLES_BLUEPRINT
 
 APP = Flask(__name__)
 APP.secret_key = os.urandom(16)
 register_db(APP)
 APP.register_blueprint(LOGIN_BLUEPRINT, url_prefix="/login")
 APP.register_blueprint(INDEX_BLUEPRINT)
-APP.register_blueprint(TABLE_STATUS_BLUEPRINT)
+APP.register_blueprint(TABLES_BLUEPRINT)
