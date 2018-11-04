cf5beac0548a78618cf2a8fc6f4aed5bb9e7cd1a
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-04 12:15:16 +1100

    wew

--- web/static/js/pa.js
+++ web/static/js/pa.js
@@ -6,39 +6,32 @@ var irs = (function() {
     function chrome() {
         /* do some magical checking that chrome is available */
         if (typeof (window.SpeechRecognition || window.webkitSpeechRecognition) === "undefined") {
-            return null
+            return null;
         }
 
         //Voice Recognition
         function listen(words, timeout, callback) {
-            
             var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
             var recognition = new SpeechRecognition();
             
             recognition.continuous = true;
-            
             recognition.onresult = function(event) {
-
-                txt = event.results[event.resultIndex][0].transcript
+                txt = event.results[event.resultIndex][0].transcript;
                 console.log(txt);
-
-                recognised = txt.split(" ")
-				
-
+                recognised = txt.split(" ");
                 for (word in recognised){
                     x = words.indexOf(recognised[word]);
                     if (x!=-1){
-						if (callback){
-							callback(words.indexOf(recognised[word]));	
-						}
-						callback = null;
+                        if (callback){
+                            callback(words.indexOf(recognised[word]));	
+                        }
+                        callback = null;
                     }
-                
                 }
-				if (x == -1){
-					if (callback){callback(x);}					
-					callback = null;
-				}
+                if (x == -1){
+                    if (callback){callback(x);}					
+                    callback = null;
+                }
                 recognition.stop();
             }
             
@@ -50,45 +43,42 @@ var irs = (function() {
                     callback = null;
                 }
             }, timeout);
-
         }
         
         //Image Capture Code
         function gotMedia(mediaStream) {
-        mediaStreamTrack = mediaStream.getVideoTracks()[0];
-        imageCapture = new ImageCapture(mediaStreamTrack);
-        console.log(imageCapture);
-        
+            mediaStreamTrack = mediaStream.getVideoTracks()[0];
+            imageCapture = new ImageCapture(mediaStreamTrack);
+            console.log(imageCapture);
         }
-		
-		function drawCanvas(canvas, img) {
-		  canvas.width = getComputedStyle(canvas).width.split('px')[0];
-		  canvas.height = getComputedStyle(canvas).height.split('px')[0];
-		  let ratio  = Math.min(canvas.width / img.width, canvas.height / img.height);
-		  let x = (canvas.width - img.width * ratio) / 2;
-		  let y = (canvas.height - img.height * ratio) / 2;
-		  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-		  canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
-			  x, y, img.width * ratio, img.height * ratio);
-		}
-		
-		
-		
+        
+        function drawCanvas(canvas, img) {
+            canvas.width = getComputedStyle(canvas).width.split('px')[0];
+            canvas.height = getComputedStyle(canvas).height.split('px')[0];
+            let ratio  = Math.min(canvas.width / img.width, canvas.height / img.height);
+            let x = (canvas.width - img.width * ratio) / 2;
+            let y = (canvas.height - img.height * ratio) / 2;
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
+                x, y, img.width * ratio, img.height * ratio);
+        }
+        
+        
+        
         function takePhoto(callback){
-            
             console.log("Say cheese")
-			
-			var htmlCanvas = document.createElement("canvas");
-			htmlCanvas.width = 1280;
-			htmlCanvas.height = 720;
-			htmlCanvas.style.visibility = "hidden";
-			document.body.appendChild(htmlCanvas);
-			imageCapture.grabFrame()
-			  .then(imageBitmap => {
-				drawCanvas(htmlCanvas, imageBitmap);
-				callback(htmlCanvas.toDataURL())
-			  })
-        .catch(error => console.log(error));
+            
+            var htmlCanvas = document.createElement("canvas");
+            htmlCanvas.width = 1280;
+            htmlCanvas.height = 720;
+            htmlCanvas.style.visibility = "hidden";
+            document.body.appendChild(htmlCanvas);
+            imageCapture.grabFrame()
+            .then(imageBitmap => {
+                drawCanvas(htmlCanvas, imageBitmap);
+                callback(htmlCanvas.toDataURL())
+            })
+            .catch(error => console.log(error));
         }
 
         navigator.mediaDevices.getUserMedia({video: true})
@@ -100,7 +90,7 @@ var irs = (function() {
             listen: listen,
             say: js_speak,
             identify: function() {
-                return 2
+                return 2;
             }
         }
     }
@@ -108,7 +98,7 @@ var irs = (function() {
     function robot() {
         //check if we are running on the robot
         if(typeof irs_raw === "undefined") {
-            return null
+            return null;
         }
         /* setup the robot callback handlers */
         irs_raw.phraseSuccess = function(res) {
@@ -128,14 +118,14 @@ var irs = (function() {
             },
             say: js_speak,
             listen: function(_, _, cb) {
-                console.error('irs.listen is not available on this platform. use chrome')
-                cb(-3)
+                console.error('irs.listen is not available on this platform. use chrome');
+                cb(-3);
             },
             photo: function(cb) {
-                console.error('irs.photo is not available on this platform. use chrome')
-                cb(null)
+                console.error('irs.photo is not available on this platform. use chrome');
+                cb(null);
             }
         }
     }
-    return robot() || chrome() || basic()
+    return robot() || chrome() || basic();
 })()
