32f8035ce9a38d6dec477395e45bd596bdfbfc92
Carol Alex <b-blackallover@hotmail.com>
2018-11-06 12:57:17 +1100

    forgot to commit this file

--- web/__init__.py
+++ web/__init__.py
@@ -1,20 +1,22 @@
 import os
-from flask import (
-    Flask, render_template
-)
+from flask import Flask
 
 from web.db import register as register_db
-from web.db import db
 from web.login import LOGIN_BLUEPRINT
 from web.index import INDEX_BLUEPRINT
+from web.robot import ROBOT_BLUEPRINT
 from web.tables import TABLES_BLUEPRINT
+#from web.welcome import WELCOME_BLUEPRINT
 
 APP = Flask(__name__)
 APP.secret_key = os.urandom(16)
 register_db(APP)
 APP.register_blueprint(LOGIN_BLUEPRINT, url_prefix="/login")
 APP.register_blueprint(INDEX_BLUEPRINT)
+APP.register_blueprint(ROBOT_BLUEPRINT)
 APP.register_blueprint(TABLES_BLUEPRINT)
+#APP.register_blueprint(WELCOME_BLUEPRINT)
+
 
 
 @APP.errorhandler(Exception)
