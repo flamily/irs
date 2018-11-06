e35ee332b05649984f3718e413324fa68ae8a732
chrisstime <christine.vinaviles@gmail.com>
2018-11-06 17:51:30 +1100

    flake8 y u do dis

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,5 @@
 import os
-from flask import (
-    Flask, render_template
-)
-
+from flask import Flask, render_template
 from web.db import register as register_db
 from web.db import db
 from web.login import LOGIN_BLUEPRINT
