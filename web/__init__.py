c7e78018decdfd52edc75e93677bad0c284271cc
Carol Alex <b-blackallover@hotmail.com>
2018-11-07 10:49:22 +1100

    minor tweaks, comments removed etc.

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template
+from web.db import db
 from web.db import register as register_db
 from web.login import LOGIN_BLUEPRINT
 from web.index import INDEX_BLUEPRINT
 from web.robot import ROBOT_BLUEPRINT
 from web.tables import TABLES_BLUEPRINT
-#from web.welcome import WELCOME_BLUEPRINT
 
 APP = Flask(__name__)
 APP.secret_key = os.urandom(16)
@@ -14,7 +14,6 @@ APP.register_blueprint(LOGIN_BLUEPRINT, url_prefix="/login")
 APP.register_blueprint(INDEX_BLUEPRINT)
 APP.register_blueprint(ROBOT_BLUEPRINT)
 APP.register_blueprint(TABLES_BLUEPRINT)
-#APP.register_blueprint(WELCOME_BLUEPRINT)
 
 
 
