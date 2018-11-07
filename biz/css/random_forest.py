55783c771c3d5cff893c91760c9cf0a04dc35a48
jjbdlsantos <jjbdlsantos@gmail.com>
2018-11-07 18:54:20 +1100

    PACKAGE IMPORTS

--- biz/css/random_forest.py
+++ biz/css/random_forest.py
@@ -12,14 +12,15 @@ def build_Random_Forest():
     rfdf = pd.read_csv(emotion_training_set)
     target_column = rfdf['satisfaction']
     del rfdf['satisfaction']
-    xTrain, xTest, yTrain, yTest = train_test_split(rfdf, target_column, test_size = 0.5, stratify=target_column, random_state=736251)
+    xTrain, xTest, yTrain, yTest = train_test_split(rfdf, target_column, test_size=0.5, 
+                                                    stratify=target_column, random_state=736251)
     irs_rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=487368)
     irs_rf.fit(xTrain, yTrain)
+    accuracy_score(yTest, irs_rf.predict(xTest))
     return irs_rf
 
 def RF_To_File(model):
     joblib.dump(model, RF_model_file)
-    return
 
 def load_RF_File():
     return joblib.load(RF_model_file)
