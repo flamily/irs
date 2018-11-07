ecbb5dafff658430810163d0e56341bba94e2a76
jjbdlsantos <jjbdlsantos@gmail.com>
2018-11-07 14:57:18 +1100

    added weighting to prediction, fixed random forest dataframe bug, added in basic model, added in training set

--- biz/css/random_forest.py
+++ biz/css/random_forest.py
@@ -1,26 +1,25 @@
 import pandas as pd
-import pickle
 
-from sklearn import datasets
+from sklearn.externals import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-def build_Random_Forest():
-    rfdf = pd.read_csv(facial_expression_data_path)
-    target_column = rfdf['satisfaction']
-    xTrain, xTest, yTrain, yTest = train_test_split(rfdf, target_column, test_size = 0.5, stratify=rfdf['satisfaction'], random_state=123456)
-    irs_rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=123456)
-    irs_rf.fit(xTrain, yTrain)
+RF_model_file = 'RF_model.joblib'
+emotion_training_set = 'emotion_training_set.csv'
 
+def build_Random_Forest():
+    rfdf = pd.read_csv(emotion_training_set)
+    target_column = rfdf['satisfaction']
+    del rfdf['satisfaction']
+    xTrain, xTest, yTrain, yTest = train_test_split(rfdf, target_column, test_size = 0.5, stratify=target_column, random_state=736251)
+    irs_rf = RandomForestClassifier(n_estimators=100, oob_score=True, random_state=487368)
+    irs_rf.fit(xTrain, yTrain)
     return irs_rf
 
 def RF_To_File(model):
-    RF_pkl = open(RF_Pickle_Path, 'wb')
-    pickle.dump(model, RF_pkl)
-    RF_pkl.close()
+    joblib.dump(model, RF_model_file)
     return
 
 def load_RF_File():
-    RF_pkl = open(RF_Pickle_Path, 'rb')
-    return pickle.load(RF_pkl)
+    return joblib.load(RF_model_file)
--- biz/css/reduction.py
+++ biz/css/reduction.py
@@ -1,6 +1,14 @@
 from random_forest import load_RF_File
 
+negative_emotions = ['anger', 'contempt', 'disgust', 'fear', 'sadness']
+
 def apply_reduction(raw_results):
     emotions = raw_results[0]["faceAttributes"]["emotion"]
+    emotion_weight_key = max(emotions, key=emotions.get)
+    emotion_weight = emotions[emotion_weight_key]
+    if emotion_weight_key in negative_emotions:
+        emotion_weight *= -1
     model = load_RF_File()
-    return model.prediction(emotions)[0]
+    to_predict = [list(emotions.values())]
+    prediction = model.prediction(to_predict) + emotion_weight
+    return prediction if prediction < 10 else 10
