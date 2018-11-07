9986e7e990dd7cbef40d23ae6b12f82a3a1c7157
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-07 13:02:49 +1100

    yeet

--- web/robot.py
+++ web/robot.py
@@ -1,14 +1,9 @@
-from flask import (
-    Blueprint
-)
-
+from flask import Blueprint
 from web.decorators import (
     login_required, templated
 )
 
 
-# Reference for blueprints here:
-# http://flask.pocoo.org/docs/1.0/blueprints/
 ROBOT_BLUEPRINT = Blueprint('robot', __name__, template_folder='templates')
 
 
@@ -33,7 +28,7 @@ def party():
     return dict(page_title='Robot - Select Party Size')
 
 
-@ROBOT_BLUEPRINT.route('/robot/table', methods=['POST', 'GET'])
+@ROBOT_BLUEPRINT.route('/robot/table', methods=['GET'])
 @templated(template='robot-table-availability.html')
 @login_required()
 def tableAvailability():
