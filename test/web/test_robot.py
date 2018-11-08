32c353074d844ffbdfec36664b7a991889e3fcde
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-09 00:51:41 +1100

    Connect Robot with Biz (#108)
    * robot flow & upload image
    
    * cleanboi
    
    * yew
    
    * clean & tidy
    
    * pushy mc pushface
    
    * smal

--- test/web/test_robot.py
+++ test/web/test_robot.py
@@ -1,29 +1,14 @@
-import biz.manage_staff as ms
-from biz.staff import Permission
-
-
-def __spoof_user(client):
-    """A user must be present and 'logged in'.
-
-    :param client: The client running the flask app.
-    :return: staff_id
-    """
-    pool = client.testing_db_pool
-    conn = pool.getconn()
-    s_id = ms.create_staff_member(
-        conn, 'rrobot', 'password', ('Roberto', 'Robot'),
-        Permission.robot
-    )
-    conn.commit()
-    pool.putconn(conn)
-    with client.session_transaction() as sess:
-        sess['username'] = 'rrobot'
-    return s_id
+from biz.manage_restaurant import create_restaurant_table
+from biz.restaurant_table import (
+    Shape, Coordinate
+)
+import pytest
+from test.web.helper import spoof_user
 
 
 def test_index(client):
     """Test that welcome endpoint can be hit."""
-    __spoof_user(client)
+    spoof_user(client)
     result = client.get('/robot')
     assert result.status_code == 200
     assert b'Welcome' in result.data
@@ -31,27 +16,59 @@ def test_index(client):
 
 def test_party_size(client):
     """Test that select_party_size endpoint can be hit."""
-    __spoof_user(client)
-    result = client.get('/robot/party-size')
+    spoof_user(client)
+    result = client.get('/robot/party')
     assert result.status_code == 200
 
 
 def test_table(client):
     """Test that robot_table endpoint can be hit."""
-    __spoof_user(client)
+    spoof_user(client)
     result = client.get('/robot/table')
     assert result.status_code == 200
 
 
 def test_full(client):
     """Test that robot-table-full endpoint can be hit."""
-    __spoof_user(client)
+    spoof_user(client)
     result = client.get('/robot/full')
     assert result.status_code == 200
 
 
 def test_proceed(client):
     """Test that table-confirmation endpoint can be hit."""
-    __spoof_user(client)
+    spoof_user(client)
     result = client.get('/robot/proceed')
     assert result.status_code == 200
+
+
+@pytest.mark.parametrize('status_code, form', [
+    (200, dict(
+        group_size=1,
+        photo='bingo',
+    )),
+    (400, dict(
+        group_size=1,
+    )),
+    (400, dict(
+        photo='bingo',
+    ))
+])
+def test_reserve(client, status_code, form):
+    """Test that robot-table-full endpoint can be hit."""
+    sid = spoof_user(client)
+    c = Coordinate(x=0, y=3)
+    pool = client.testing_db_pool
+    conn = pool.getconn()
+    tid, _ = create_restaurant_table(conn, 10, c, 1, 5, Shape.rectangle, sid)
+    conn.commit()
+    pool.putconn(conn)
+
+    form['table_id'] = tid
+
+    result = client.post('/robot/table/reserve',
+                         data=form,
+                         follow_redirects=True)
+    print(dir(result))
+    print(result.location)
+    assert result.status_code == status_code
