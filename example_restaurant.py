054f861bd19a569361774ab7ec4195e367556b33
arosspope <andrew.pope456@gmail.com>
2018-11-09 07:01:22 +1100

    better spoof timing for the most recent events

--- example_restaurant.py
+++ example_restaurant.py
@@ -245,18 +245,22 @@ def __recent_restaurant_state(tids):
         # Choose random staff member to do transaction
         sid = STAFF[random.randint(0, len(STAFF)-1)]
 
-        # Choose a random action to apply to the table - start at 9am of today
+        # Choose a random action to apply to the table
         choice = random.randint(0, 3)
-        exp_dt = datetime.utcnow().replace(hour=9, minute=1)
+        now = datetime.utcnow()
         if choice == 1:
+            start_dt = now + timedelta(hours=-1)
             print("table ({}) setup : reserved and ordered".format(tid))
-            __customer_experience(tid, sid, capacity, exp_dt)
+            __customer_experience(tid, sid, capacity, start_dt)
         elif choice == 2:
+            start_dt = now + timedelta(hours=-3)
             print("table ({}) setup : reserved, ordered and paid".format(tid))
-            __customer_experience(tid, sid, capacity, exp_dt, True)
+            __customer_experience(tid, sid, capacity, start_dt, True)
         elif choice == 3:
+            start_dt = now + timedelta(minutes=-30)
             print("table ({}) setup : maintainence".format(tid))
-            mgrest.maintain(conn, tid, sid)
+            event = mgrest.maintain(conn, tid, sid)
+            SPOOF_DATES['table_maintainence'].append((event, start_dt))
         else:
             print("table ({}) setup : ready".format(tid))
             continue  # Leave it be (available)
@@ -274,7 +278,7 @@ def __create_history(tids):
     # For each table we want to generate a sordid history
     # (only 4 days worth)
     for tid, capacity in tids:
-        for day in reversed(range(2, 6)):
+        for day in reversed(range(1, 6)):
             for hour in range(0, 8, random.randint(1, 3)):
                 # Choose a random staff memeber
                 sid = STAFF[random.randint(0, len(STAFF)-1)]
