81a5ffb2de923dd0b0ddd0e7104c728e2ec3de6d
Christine Vinaviles <christine.vinaviles@gmail.com>
2018-11-08 16:48:36 +1100

    Routing for the Robot select party and welcome Interface + tests (#75)

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,5 @@
 import os
-from flask import (
-    Flask, render_template
-)
-
+from flask import Flask, render_template
 from web.db import register as register_db
 from web.db import db
 from web.login import LOGIN_BLUEPRINT
@@ -16,8 +13,8 @@ APP.secret_key = os.environ.get("SECRET", os.urandom(16))
 register_db(APP)
 APP.register_blueprint(LOGIN_BLUEPRINT, url_prefix="/login")
 APP.register_blueprint(INDEX_BLUEPRINT)
-APP.register_blueprint(ROBOT_BLUEPRINT)
 APP.register_blueprint(TABLES_BLUEPRINT)
+APP.register_blueprint(ROBOT_BLUEPRINT)
 
 
 @APP.errorhandler(404)
