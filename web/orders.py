b83a451fbb4cda75bf4a81929ce4cabdaa25cfcc
David Niwczyk <davidniw@hotmail.com>
2018-11-09 19:09:49 +1100

    #114 adding broken post endpoint

--- web/orders.py
+++ web/orders.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, request
 )
 import biz.manage_restaurant as mr
+import biz.manage_menu as mm
 from web.db import db
 from web.decorators import (
     login_required, templated, user
@@ -18,7 +19,7 @@ def get_occupied_tables():
     occupied_tables = list()
     for table in tables:
         if mr.lookup_reservation(db, table.rt_id):
-            occupied_tables.append(table.rt_id)
+            occupied_tables.append(table)
     return dict(page_title='Order - Select Table',
                 tables=occupied_tables)
 
@@ -37,9 +38,5 @@ def get_order():
 @ORDERS_BLUEPRINT.route('/order/new', methods=['POST'])
 @login_required()
 def create_order():
-    table_id = request.json['table_id']
-    rid = mr.lookup_reservation(db, table_id)
-    if not rid:
-        return "No reservation found for table: " + table_id
-
-    return str(user.s_id)
+    
+    return str(request.json)
