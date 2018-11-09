4a7dd527a8fe20edd290cf4b90a390f40a9b41ed
NDGogo <andy.go@student.uts.edu.au>
2018-11-09 17:58:02 +1100

    moved code to manage_satisfaction

--- biz/css/manage_satisfaction.py
+++ biz/css/manage_satisfaction.py
@@ -43,3 +43,27 @@ def lookup_satisfaction(db_conn, event_id, reservation_id):
             return None
         score = curs.fetchone()[0]
     return score
+
+
+"""
+    * CSS values across time (daily, weekly, monthly) entry vs exit
+    * Dropdown for specific timing per filter
+    * Average CSS for specified time period
+    * Average CSS per staff member
+    * Graph historic CSS values for staff member and show on graph.
+    * Trend of selected meals from group (nice to have)
+"""
+
+
+def retrieve_avg_score(db_conn):
+    with db_conn.cursor() as curs:
+        curs.execute(
+            "SELECT *"
+            "FROM satisfaction s"
+            "JOIN reservation r on r.reservation_id = s.reservation_id"
+            "LIMIT 10"
+        )
+        if curs.rowcount != 1:
+            return None
+        avg_score = curs.fetchone()[0]
+    return avg_score
