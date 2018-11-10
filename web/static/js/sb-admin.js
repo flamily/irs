b87604bf599d48392293ffeec112d01512f2b05c
inPhamous <inphamous@hotmail.com.au>
2018-11-10 13:03:21 +1100

    Exit-interface Quality of life changes (#117)
    * Added status text to table buttons
    
    * Fixed duplication of button status text ids
    
    * Fixed photo capture not being sent

--- web/static/js/sb-admin.js
+++ web/static/js/sb-admin.js
@@ -78,18 +78,13 @@
     $('.modal-body').text('You are confirming a table for ' + size + ' people. Please select confirm to continue or cancel to enter again.')
   });
 
+  var pageElement;
+
   var photoCallback = function updatePhotoField(encodedImage){
     $("#customerImg").val(encodedImage);
-  }
-
-  $('#updateTable').find('input').click(function(event){
-    event.preventDefault();
-
-    if($(this).val() == 'Pay')
-      irs.photo(photoCallback);
 
     $.ajax({
-      url: $(this)[0].getAttribute('formaction'),
+      url: pageElement[0].getAttribute('formaction'),
       data: $('#tableInfo').serialize(),
       type: 'POST',
       success: function(response) {
@@ -100,6 +95,31 @@
         console.log(error.statusText);
       }
     });
+
+  }
+
+  $('#updateTable').find('input').click(function(event){
+    event.preventDefault();
+
+    if($(this).val() == 'Pay'){
+      pageElement = $(this);
+      irs.photo(photoCallback);
+    }
+    else{
+      $.ajax({
+        url: $(this)[0].getAttribute('formaction'),
+        data: $('#tableInfo').serialize(),
+        type: 'POST',
+        success: function(response) {
+          updateTableStatuses(response);
+          $('#statusModal').modal('hide');
+        },
+        error: function(error) {
+          console.log(error.statusText);
+        }
+      });
+    }
+
     // Clear out image field to prevent issues with other buttons
     $("#customerImg").val('');
   });
@@ -117,12 +137,14 @@
     switch(response.status){
       case 'available':
         $(element).addClass('available-table');
-        $(element).data('status', 'available')
+        $(element).children('p').first().text('available');
+        $(element).data('status', 'available');
         break;
 
       case 'unavailable':
         $(element).addClass('unavailable-table');
-        $(element).data('status', 'unavailable')
+        $(element).children('p').first().text('unavailable');
+        $(element).data('status', 'unavailable');
         break;
     }
   }
