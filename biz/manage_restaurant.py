b5e7e5b57a12ef135d7cf94e84229237b42b3dec
Carol Alex <b-blackallover@hotmail.com>
2018-11-11 17:04:53 +1100

    test fixes

--- biz/manage_restaurant.py
+++ biz/manage_restaurant.py
@@ -289,7 +289,12 @@ def get_available_tables(db_conn, capacity):
             rt_list.append(
                 RestaurantTable(
                     rt_id=table[0],
-                    capacity=table[1]
+                    capacity=table[1],
+                    coordinate=Coordinate(x=table[2], y=table[3]),
+                    width=table[4],
+                    height=table[5],
+                    shape=Shape(table[6]),
+                    latest_event=Event(table[7])
                 )
             )
     return rt_list
