cc29a49c56089f7192d14f94345807fa353a2144
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-11 15:16:00 +1100

    injecto the key

--- biz/css/emotion_recognition.py
+++ biz/css/emotion_recognition.py
@@ -1,8 +1,9 @@
 import cognitive_face as CF
+import config
 
 
 def detect_from_url(url):  # pragma: no cover
-    KEY = 'fcef05be3b9f440f9e38dfb675b07de6'
+    KEY = config.cf_api_key()
     BASE = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0'
     CF.Key.set(KEY)
     CF.BaseUrl.set(BASE)
