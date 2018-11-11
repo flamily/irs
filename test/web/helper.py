276c89d093ad4237d634228ea740865138e07df7
David Niwczyk <davidniw@hotmail.com>
2018-11-11 17:50:13 +1100

    just some tests

--- test/web/helper.py
+++ test/web/helper.py
@@ -1,5 +1,7 @@
 import biz.manage_staff as ms
+import biz.manage_restaurant as mg
 from biz.staff import Permission
+from biz.restaurant_table import Coordinate, Shape
 
 
 def spoof_user(client):
@@ -19,3 +21,33 @@ def spoof_user(client):
     with client.session_transaction() as sess:
         sess['username'] = 'rrobot'
     return s_id
+
+
+def spoof_tables(client, n, staff_id, reserve=False):
+    """Load a series of restaurant tables.
+
+    :param client: The client running the flask app.
+    :param n: The number of restaurant_tables to create.
+    :param staff_id: The id of the staff member creating the tables.
+    :param reserve: Mark the tables as being reserved.
+    :return: ([t1_id, t2_id ... tn_id])
+    """
+    pool = client.testing_db_pool
+    conn = pool.getconn()
+    tables = []
+    for _ in range(0, n):
+        tables.append(
+            mg.create_restaurant_table(
+                conn, 3, Coordinate(x=0, y=3), 1,
+                5, Shape.rectangle, staff_id
+            )[0]
+        )
+    conn.commit()
+
+    if reserve:
+        for table in tables:
+            mg.create_reservation(conn, table, staff_id, 2)
+        conn.commit()
+
+    pool.putconn(conn)
+    return tables
---
+++ test/web/test_orders.py
@@ -0,0 +1,84 @@
+import pytest
+import biz.manage_menu as mm
+import biz.manage_restaurant as mr
+from test.web.helper import spoof_user, spoof_tables
+
+
+def __spoof_menu_items(client):
+    """Load a series of restaurant menu items.
+
+    :param client: The client running the flask app.
+    """
+    pool = client.testing_db_pool
+    conn = pool.getconn()
+    menu_item = mm.create_menu_item(
+                conn, 'food stuffs', 'good stuff', 2.99
+            )
+    conn.commit()
+
+    pool.putconn(conn)
+    return menu_item
+
+
+def test_order_index(client):
+    """Test that welcome endpoint can be hit."""
+    spoof_user(client)
+    result = client.get('/order/new', follow_redirects=True)
+    assert result.status_code == 200
+    assert b'Place An Order' in result.data
+
+
+def test_order_menu_items(client):
+    """Test that the new order page is populated with menu items."""
+    spoof_user(client)
+    menu_item = __spoof_menu_items(client)
+    result = client.get('/order/new', follow_redirects=True)
+
+    expect = 'data-menu_item_id="{}"'.format(menu_item)
+    assert expect in str(result.data).replace(' ', '')
+
+
+def test_order_tables(client):
+    """Test that the new order page is populated tables."""
+    num_tables = 1
+    sid = spoof_user(client)
+    table = spoof_tables(client, num_tables, sid, True)[0]
+    result = client.get('/order/new', follow_redirects=True)
+    expect = 'data-menu_table_id="{}"'.format(table)
+    assert expect in str(result.data).replace(' ', '')
+
+
+def test_order_created(client):
+    """Test that an order can be created."""
+    num_tables = 1
+    sid = spoof_user(client)
+    table = spoof_tables(client, num_tables, sid, True)[0]
+    menu_item = __spoof_menu_items(client)
+    form = dict([('table_id', table), (menu_item, 1)])
+    pool = client.testing_db_pool
+    conn = pool.getconn()
+    r_id = mr.lookup_reservation(conn, table)
+    pool.putconn(conn)
+    result = client.post('/order/new',
+                         data=form,
+                         follow_redirects=True)
+
+    expect = 'data-reservation_id="{}"'.format(r_id)
+    assert expect in str(result.data).replace(' ', '')
+
+
+def test_list_menu_items(client):
+    """Test that restaurant tables from db are being returned."""
+    num_tables = 1
+    sid = spoof_user(client)
+    table = spoof_tables(client, num_tables, sid, True)[0]
+    pool = client.testing_db_pool
+    conn = pool.getconn()
+    r_id = mr.lookup_reservation(conn, table)
+    o_id = mr.lookup_order(conn, r_id)
+    pool.putconn(conn)
+    form = dict([('rid', r_id)])
+    result = client.post('/order/get',
+                         data=form,
+                         follow_redirects=True)
+    assert o_id in str(result)
