0eae244800eee5ea3d78170634d6f0cc495fd894
Robin Wohlers-Reichel <robin.wohlersreichel@gmail.com>
2018-11-13 19:25:02 +1100

    testy mc testface

--- test/web/test_reporting_api.py
+++ test/web/test_reporting_api.py
@@ -46,39 +46,58 @@ def test_invalid_usage(client, url, err_msg):
     assert err_msg == json.loads(result.data)['message']
 
 
-@pytest.mark.parametrize('url, json_keys', [
+@pytest.mark.parametrize('url, json_cardinality', [
+    # json_cardinality is the number of items
+    #   expected in a list at the key
+    # None is a special case where we don't expect a list
+
     # get_customer_report
     ('/api/reporting/Customer/date?dateString=2018-01-01',
-        ['average', 'data', 'labels', 'scores']),
-    # ('/api/reporting/Customer/not_date?dateString=yew',
-    #     'Invalid date format provided'),
+        {
+            'average': None,
+            'data': 3,
+            'labels': 3,
+            'scores': 3,
+        }),
 
-    # # get_staff_report
-    # ('/api/reporting/Staff/1/date',
-    #     'No date string provided'),
-    # ('/api/reporting/Staff/1/not_date?dateString=yew',
-    #     'Invalid date format provided'),
-    # ('/api/reporting/Staff/staff_id/date?dateString=yew',
-    #     'Invalid staff_id provided'),
+    # get_staff_report
+    ('/api/reporting/Staff/1/date?dateString=2018-01-01',
+        {
+            'average': None,
+            'data': 3,
+            'labels': 3,
+            'scores': 3,
+        }),
 
-    # # get_menu_score
-    # ('/api/reporting/Menu/1/date',
-    #     'No date string provided'),
-    # ('/api/reporting/Menu/1/not_date?dateString=yew',
-    #     'Invalid date format provided'),
-    # ('/api/reporting/Menu/staff_id/date?dateString=yew',
-    #     'Invalid menu_id provided'),
+    # get_menu_score
+    ('/api/reporting/Menu/1/date?dateString=2018-01-01',
+        {
+            'average': None,
+            'data': 3,
+            'labels': 3,
+            'scores': 3,
+        }),
 
-    # # staff_missing_error
-    # ('/api/reporting/Staff//date',
-    #     'Missing staff_id from request'),
+    # get_staff_and_menu_items
+    ('/api/reporting/list_items',
+        {
+            'menu': 3,
+            'staff': 1,
+        }),
 
-    # # menu_missing_error
-    # ('/api/reporting/Menu//date',
-    #     'Missing menu_id from request'),
+    # get_latest_entry_time
+    ('/api/time/get_latest',
+        {
+            'data': None,
+        }),
 
+    # get_list_of_years
+    ('/api/time/get_years',
+        {
+            'years': 1,
+        }),
 ])
-def test_endpoints(client, url, json_keys):
+def test_endpoints(client, url, json_cardinality):
     pool = client.testing_db_pool
     db_connection = pool.getconn()
     t, staff = h.spoof_tables(db_connection, 3)
@@ -105,6 +124,12 @@ def test_endpoints(client, url, json_keys):
     result = client.get(url)
     assert 200 == result.status_code
     payload = json.loads(result.data)
-    assert len(json_keys) == len(payload)
-    for k in json_keys:
-        assert payload.get(k, False)
+    assert len(json_cardinality) == len(payload)
+    for key, cardinality in json_cardinality.items():
+        thing = payload.get(key, False)
+        if cardinality is None:
+            # we just check there is something
+            assert thing
+        else:
+            # we check the length
+            assert cardinality == len(thing)
