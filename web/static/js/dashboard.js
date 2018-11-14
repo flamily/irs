dccaa86a91a07800b85ae5e0ed32040da897cada
Jacob V <jacobvorreiter@gmail.com>
2018-11-14 13:56:34 +1100

    Dashboard implementation (#126)
    * initial things
    
    * dashboard with filters for date selection
    
    * some fun randomly generated data added to the graph on filter button press
    
    * moved code to manage_satisfaction
    
    * added satisfaction record into db
    
    * added average css per period and per staff
    
    * turning useless comment into useful comment
    
    * turning useless comment into useful comment
    
    * use AVG() SQL function instead for avg_score
    
    * cast avg_score to int
    
    * Added reporting queries allowing db to be searched using two dates, additional helper functions for date, week, month and year
    
    * retrieving historic css from time filter
    
    * reverting back to python avg_score calc cause Travis
    
    * avg_css for all staff and updated comments per feedback
    
    * moving charts :O
    
    * Sorting method to ensure timestamps were displayed correctly, removed some play code
    
    * re-enabled error handling
    
    * added avg_css for specified menu item and cleaned up SQL queries in previous functions
    
    * accidentally removed test_avg_css_all_staff test
    
    * Return all values from DB for more comprehensive reporting
    
    * removed unused app folder, removed unused code
    
    * removed duplicate functionality + using SQL AVG() again with proper tests
    
    * order query by event_dt
    
    * trying to get DataTables to work and be quick
    
    * allow sort by date
    
    * Faster loading graphs, inserting date types instead of strings for sorting, more UI work
    
    * Cleanup and nice to have features added
    
    * Cleanup, adding in missing functions after merge, docstrings for JS and python files
    
    * Updated to pylint and flake8 standards to make Travis happy
    
    * More pylint fixes
    
    * more linting fixes
    
    * omfg please travis
    
    * Added error handling and input validation
    
    * Added update call when switching to year as there may only be one year
    
    * More error handling, linter cleanup, files added for test coverage wip
    
    * rollback da boi
    
    * butcher
    
    * move move
    
    * Removed assert from code, based on preliminary testing
    
    * Fixed code for reporting move to biz module
    
    * Testing so far
    
    * linter and tests
    
    * closer...
    
    * better debugging
    
    * satisfaction db tests
    
    * Removed unused helper function
    
    * wew
    
    * uncomment test
    
    * testy mc testface
    
    * 10.00/10 code
    
    * dups—
    
    * almost there with the tests
    
    * Final testing and linting
    
    * wtf is this m80
    
    * Added table heading
    
    * Added docstrings and module comments for PR review
    
    * More function commenting
    
    * moar comments
    
    * Last comments hopefully

---
+++ web/static/js/dashboard.js
@@ -0,0 +1,161 @@
+/**
+* All functions for html generation & endpoint data calls to create a seemless, one page dashboard solution
+*
+* Author: Jacob Vorreiter
+* Date: 12/11/2018
+*/
+
+var options = "";
+var mode = "Customer";
+
+/**
+* One time endpoint call make ready the options for a year selector
+*/
+function populateYearOptions(){
+  function callback(data){
+    $.each(data.years, function(k, v){
+      options += "<option value='" + v + "'>" + v + "</option>";
+    });
+  }
+
+  if (options.length == 0){
+    $.getJSON('/api/time/get_years', callback);
+  }
+}
+
+/**
+* JS HTML generator for date format change (date, week, month, year)
+* @param {String} inputType String for date format change
+*/
+function changeDatePicker(inputType){
+  var decoration = "id='date-picker' class='form-control form-control-sm' onchange='updateAll()'";
+  if (inputType == "year"){
+    var selector = document.getElementById("date-picker-module");
+    selector.innerHTML = "<select name='year'" + decoration + ">" + options + "</select>";
+    updateAll();
+  } else {
+    document.getElementById("date-picker-module").innerHTML = "<input name='" + inputType + "' type=" + inputType + " " + decoration + ">";
+  }
+}
+
+/**
+* Generates the string to call the correct endpoint
+* @return {String} A formatted, populated api call (eg /api/reporting/Customer/week?dateString=2018-W45)
+*/
+function getAPIString(){
+  var date_type = document.getElementById('date-picker').getAttribute("name");
+  var date_string = $("#date-picker").val();
+  var item_selector_value = $("select#item-selector>option:selected").attr("value");
+
+  if (typeof item_selector_value == "undefined"){
+    item_selector_value = 1;
+  }
+  if (mode == "Customer"){
+      var api_string = '/api/reporting/' + mode + '/' + date_type + '?dateString=' + date_string;
+  } else {
+    var api_string = '/api/reporting/' + mode + '/' + item_selector_value + '/' + date_type + '?dateString=' + date_string;
+  }
+
+  return api_string;
+}
+
+/**
+* Validates date input data before sending it to the sql to avoid errors
+*
+* @return {Boolean} The success of whether the string matches the regex
+*/
+function validate(){
+  var date_picker = document.getElementById("date-picker");
+  var date_picker_label = document.getElementById("date-picker-label");
+  var date_string = $("#date-picker").val();
+  var date_regex = /[0-9]{4}\-0[0-9]|1[0-2]\-[0-2][0-9]|3[0-1]/;
+  var week_regex = /[0-9]{4}\-W[0-4][0-9]|W5[0-2]/;
+  var month_regex = /[0-9]{4}\-0[0-9]|1[0-2]/;
+  var year_regex = /[0-9]{4}/;
+  if (date_regex.test(date_string) == true ||
+      week_regex.test(date_string) == true ||
+      month_regex.test(date_string) == true ||
+      year_regex.test(date_string) == true){
+    date_picker.classList.remove("is-invalid");
+    date_picker.classList.add('is-valid');
+    date_picker_label.classList.remove('text-danger');
+    return true;
+  } else {
+    date_picker.classList.remove('is-valid');
+    date_picker.classList.add('is-invalid');
+    date_picker_label.classList.add('text-danger');
+    return false;
+  }
+}
+
+/**
+* Updates the chart and table, calling appropriate functions to get data from the endpoint
+*/
+function updateAll(){
+  if (validate() == true){
+    updateChart();
+    updateTable();
+  }
+}
+
+/**
+* Updates the Chart.js chart data by reading data from the endpoint
+*/
+function updateChart(){
+  api_string = getAPIString();
+
+  $.getJSON(api_string, function(data){
+    document.getElementById("average_score").innerHTML = "<strong>Average Score: " + data.average + "</strong>"
+    myLineChart.data.datasets[0].data = data.scores;
+    myLineChart.data.labels = data.labels;
+    myLineChart.update();
+    document.getElementById('chart-updated').innerHTML = "Updated " + new Date();
+  });
+}
+
+/**
+* Updates the DataTable table by reading data from the endpoint and reloading the DataTable ajax url
+*/
+function updateTable(){
+  api_string = getAPIString();
+  myDataTable.ajax.url(api_string).load();
+  document.getElementById('table-updated').innerHTML = "Updated " + new Date();
+}
+
+/**
+* Obtains selector data from the endpoint and populates the select with options according to the current mode (ie Staff or Menu)
+*/
+function populateListItems(){
+  $.getJSON('/api/reporting/list_items', function(data){
+    document.getElementById('item-selector').innerHTML = "";
+    $.each(data[mode.toLowerCase()], function(k, v){
+      document.getElementById('item-selector').innerHTML += "<option value='" + v.value + "'>" + v.name + "</option>";
+    });
+  });
+}
+
+/**
+* Changes the Graph mode to view different satisfaction visualisations (ie Customer, Staff, Menu)
+*
+* @param {HTML Element} The calling element, used to obtain name data from the element
+*/
+function changeGraphMode(elem){
+  mode = elem.name;
+  document.getElementById("graph_title").innerHTML = elem.name + " Satisfaction Graph";
+  document.getElementById("table-heading").innerHTML = elem.name + " Events";
+  if (elem.name == "Menu"){
+    document.getElementById('table-changable-head').innerHTML = "Quantity";
+    document.getElementById('table-changable-foot').innerHTML = "Quantity";
+  } else {
+    document.getElementById('table-changable-head').innerHTML = "Event Description";
+    document.getElementById('table-changable-foot').innerHTML = "Event Description";
+  }
+  if (elem.name == "Customer"){
+    document.getElementById('item-selector-area').style.display = "none";
+  } else {
+    document.getElementById('item-selector-area').style.display = "block";
+    document.getElementById('item-select-label').innerHTML = elem.name + " ID";
+    populateListItems();
+  }
+  updateAll();
+}
